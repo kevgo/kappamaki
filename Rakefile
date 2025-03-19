e1e9e133bd40f6346e66b258e34b9aa92d923a06
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-19 09:06:21 -0500

    Use double-quotes (#16)

--- Rakefile
+++ Rakefile
@@ -1,19 +1,19 @@
-require 'bundler'
-require 'bundler/gem_tasks'
-require 'rspec/core/rake_task'
+require "bundler"
+require "bundler/gem_tasks"
+require "rspec/core/rake_task"
 
 RSpec::Core::RakeTask.new :spec
 
 task default: [:lint, :spec]
 
-desc 'Fix all auto-fixable issues'
-task 'fix' do
-  sh 'bundle exec rubocop -a'
-  sh 'dprint fmt'
+desc "Fix all auto-fixable issues"
+task "fix" do
+  sh "bundle exec rubocop -a"
+  sh "dprint fmt"
 end
 
-desc 'Run linter'
-task 'lint' do
-  sh 'bundle exec rubocop'
-  sh 'dprint check'
+desc "Run linter"
+task "lint" do
+  sh "bundle exec rubocop"
+  sh "dprint check"
 end
--- spec/kappamaki_spec.rb
+++ spec/kappamaki_spec.rb
@@ -1,28 +1,27 @@
 require 'spec_helper'
 require 'kappamaki'
 
-
 describe Kappamaki do
 
   test_data = {
 
     attributes_from_sentence: {
-      'name: "Foo"'                                => { name: 'Foo' },
-      'name: "Foo" and body: "Bar"'                => { name: 'Foo', body: 'Bar' },
-      'name: "Foo", body: "Bar" and title: "Baz"'  => { name: 'Foo', body: 'Bar', title: 'Baz' },
+      'name: "Foo"' => { name: 'Foo' },
+      'name: "Foo" and body: "Bar"' => { name: 'Foo', body: 'Bar' },
+      'name: "Foo", body: "Bar" and title: "Baz"' => { name: 'Foo', body: 'Bar', title: 'Baz' },
       'name: "Foo", body: "Bar", and title: "Baz"' => { name: 'Foo', body: 'Bar', title: 'Baz' }
     },
 
     from_sentence: {
-      'one'                              => ['one'],
-      '"one"'                            => ['one'],
-      'one and two'                      => ['one', 'two'],
-      '"one" and "two"'                  => ['one', 'two'],
-      'one, two and three'               => ['one', 'two', 'three'],
-      '"one", "two" and "three"'         => ['one', 'two', 'three'],
-      'one, two, and three'              => ['one', 'two', 'three'],
-      'one, two, three and four'         => ['one', 'two', 'three', 'four'],
-      '"one", "two", "three" and "four"' => ['one', 'two', 'three', 'four']
+      'one' => ['one'],
+      '"one"' => ['one'],
+      'one and two' => %w[one two],
+      '"one" and "two"' => %w[one two],
+      'one, two and three' => %w[one two three],
+      '"one", "two" and "three"' => %w[one two three],
+      'one, two, and three' => %w[one two three],
+      'one, two, three and four' => %w[one two three four],
+      '"one", "two", "three" and "four"' => %w[one two three four]
     }
   }
 
@@ -30,12 +29,11 @@ describe Kappamaki do
     describe method_name do
       expected.each do |sentence, expected_result|
         it "parses '#{sentence}' into '#{expected_result}'" do
-          expect(Kappamaki.send method_name, sentence).to eql expected_result
+          expect(Kappamaki.send(method_name, sentence)).to eql expected_result
         end
       end
     end
   end
-
 
   describe 'symbolize_keys_deep!' do
 
