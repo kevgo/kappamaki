d34cee27c3ff72b165d285ed39a90d5f0e69a56f
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-19 09:32:10 -0500

    Gemfile kappamaki.gemspec kappamaki.rb kappamaki_spec.rb

--- kappamaki.gemspec
+++ kappamaki.gemspec
@@ -1,29 +1,23 @@
-lib = File.expand_path('lib', __dir__)
+lib = File.expand_path("lib", __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 Gem::Specification.new do |spec|
-  spec.name     = 'kappamaki'
-  spec.version  = '1.0.0'
-  spec.authors  = ['Kevin Goslar']
-  spec.email    = ['kevin.goslar@gmail.com']
-  spec.summary  = 'Tools for defining data using natural language in Cucumber steps'
-  spec.homepage = 'https://github.com/kevgo/kappamaki'
-  spec.license  = 'MIT'
-  spec.description = <<-EOF
+  spec.name     = "kappamaki"
+  spec.version  = "1.0.0"
+  spec.authors  = ["Kevin Goslar"]
+  spec.email    = ["kevin.goslar@gmail.com"]
+  spec.summary  = "Tools for defining data using natural language in Cucumber steps"
+  spec.homepage = "https://github.com/kevgo/kappamaki"
+  spec.license  = "MIT"
+  spec.description = <<-DESC
     Kappamaki is a collection of string manipulation methods
     that make parsing high-lever cucumber steps easier.
-  EOF
+  DESC
 
   spec.files         = `git ls-files -z`.split("\x0")
   spec.executables   = []
-  spec.require_paths = ['lib']
+  spec.require_paths = ["lib"]
 
-  spec.add_development_dependency 'bundler'
-  spec.add_development_dependency 'rake'
-  spec.add_development_dependency 'rspec'
-  spec.add_development_dependency 'rubocop'
-  spec.add_development_dependency 'simplecov'
-
-  spec.required_ruby_version = '>= 1.9.3'
-  spec.metadata['rubygems_mfa_required'] = 'true'
+  spec.required_ruby_version = ">= 2.0"
+  spec.metadata["rubygems_mfa_required"] = "true"
 end
--- spec/kappamaki_spec.rb
+++ spec/kappamaki_spec.rb
@@ -1,53 +1,51 @@
-require 'spec_helper'
-require 'kappamaki'
+require "spec_helper"
+require "kappamaki"
 
-describe Kappamaki do
-
+describe :attributes_from_sentence do
   test_data = {
-
-    attributes_from_sentence: {
-      'name: "Foo"' => { name: 'Foo' },
-      'name: "Foo" and body: "Bar"' => { name: 'Foo', body: 'Bar' },
-      'name: "Foo", body: "Bar" and title: "Baz"' => { name: 'Foo', body: 'Bar', title: 'Baz' },
-      'name: "Foo", body: "Bar", and title: "Baz"' => { name: 'Foo', body: 'Bar', title: 'Baz' }
-    },
-
-    from_sentence: {
-      'one' => ['one'],
-      '"one"' => ['one'],
-      'one and two' => %w[one two],
-      '"one" and "two"' => %w[one two],
-      'one, two and three' => %w[one two three],
-      '"one", "two" and "three"' => %w[one two three],
-      'one, two, and three' => %w[one two three],
-      'one, two, three and four' => %w[one two three four],
-      '"one", "two", "three" and "four"' => %w[one two three four]
-    }
+    'name: "Foo"' => { name: "Foo" },
+    'name: "Foo" and body: "Bar"' => { name: "Foo", body: "Bar" },
+    'name: "Foo", body: "Bar" and title: "Baz"' => { name: "Foo", body: "Bar", title: "Baz" },
+    'name: "Foo", body: "Bar", and title: "Baz"' => { name: "Foo", body: "Bar", title: "Baz" }
   }
-
-  test_data.each do |method_name, expected|
-    describe method_name do
-      expected.each do |sentence, expected_result|
-        it "parses '#{sentence}' into '#{expected_result}'" do
-          expect(Kappamaki.send(method_name, sentence)).to eql expected_result
-        end
-      end
+  test_data.each do |sentence, expected_result|
+    it "parses '#{sentence}' into '#{expected_result}'" do
+      expect(Kappamaki.attributes_from_sentence(sentence)).to eql expected_result
     end
   end
+end
 
-  describe 'symbolize_keys_deep!' do
-
-    it 'converts string keys to symbols' do
-      actual = { 'one' => 'two', 'alpha' => 'beta' }
-      Kappamaki.symbolize_keys_deep! actual
-      expect(actual).to eq one: 'two', alpha: 'beta'
+describe :from_sentence do
+  test_data = {
+    "one" => ["one"],
+    '"one"' => ["one"],
+    "one and two" => %w[one two],
+    '"one" and "two"' => %w[one two],
+    "one, two and three" => %w[one two three],
+    '"one", "two" and "three"' => %w[one two three],
+    "one, two, and three" => %w[one two three],
+    "one, two, three and four" => %w[one two three four],
+    '"one", "two", "three" and "four"' => %w[one two three four]
+  }
+  test_data.each do |sentence, expected_result|
+    it "parses '#{sentence}' into '#{expected_result}'" do
+      expect(Kappamaki.from_sentence(sentence)).to eql expected_result
     end
-
-    it 'converts nested keys' do
-      actual = { 'one' => { 'alpha' => 'beta' } }
-      Kappamaki.symbolize_keys_deep! actual
-      expect(actual).to eq one: { alpha: 'beta' }
-    end
-
   end
+end
+
+describe "symbolize_keys_deep!" do
+
+  it "converts string keys to symbols" do
+    actual = { "one" => "two", "alpha" => "beta" }
+    Kappamaki.symbolize_keys_deep! actual
+    expect(actual).to eq one: "two", alpha: "beta"
+  end
+
+  it "converts nested keys" do
+    actual = { "one" => { "alpha" => "beta" } }
+    Kappamaki.symbolize_keys_deep! actual
+    expect(actual).to eq one: { alpha: "beta" }
+  end
+
 end
