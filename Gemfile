1c1549d5e35871718472ad230e6fcca32a166299
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-19 10:36:54 -0500

    Format everything (#17)

--- Gemfile
+++ Gemfile
@@ -1,4 +1,10 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
 # Specify your gem's dependencies in kappamaki.gemspec
 gemspec
+
+gem "bundler"
+gem "rake"
+gem "rspec"
+gem "rubocop"
+gem "simplecov"
--- lib/kappamaki.rb
+++ lib/kappamaki.rb
@@ -1,34 +1,32 @@
+# Kappamaki provides helper methods to implement Cucumber steps
+# that define data using natural language.
 module Kappamaki
-
   # Parses a given string containing key-value pairs into a Hash
   #
   # Values must be delimited by double-quotes, like this:
   # key: "value"
-  def self.attributes_from_sentence sentence
+  def self.attributes_from_sentence(sentence)
     attributes = Kappamaki.from_sentence(sentence)
-                          .map { |piece| piece.split ': ' }
+                          .map { |piece| piece.split ": " }
                           .map { |key, value| [key.to_sym, value] }
                           .flatten
     Hash[*attributes]
   end
 
-
   # Reverse of ActiveSupport's "to_sentence" method
-  def self.from_sentence sentence
-    sentence.gsub(', and ', ', ')
-            .gsub(' and ', ', ')
-            .split(', ')
+  def self.from_sentence(sentence)
+    sentence.gsub(", and ", ", ")
+            .gsub(" and ", ", ")
+            .split(", ")
             .map { |s| s.delete '"' }
   end
 
-
   # Converts all keys in the given hash to symbols
-  def self.symbolize_keys_deep! h
-    h.keys.each do |k|
-      ks    = k.to_sym
-      h[ks] = h.delete k
-      symbolize_keys_deep! h[ks] if h[ks].is_a? Hash
+  def self.symbolize_keys_deep!(hash)
+    hash.keys.each do |k|
+      ks = k.to_sym
+      hash[ks] = hash.delete k
+      symbolize_keys_deep! hash[ks] if hash[ks].is_a? Hash
     end
   end
-
 end
